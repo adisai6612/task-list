006708879c0e08711246927c3aec31995077d92f
adisai6612 <patoleaditi4@gmail.com>
2023-11-27 12:29:44 +0530

    updated api url

--- ui/src/utils.js
+++ ui/src/utils.js
@@ -1,2 +1,2 @@
 export const API_URL =
-  "https://xlegv3aprnnpvadb2wdu2d3e740lwtyn.lambda-url.us-west-1.on.aws/task";
+  "https://abneo52v6fzhegyayurg2qrmgq0eajpw.lambda-url.us-west-1.on.aws/task";
